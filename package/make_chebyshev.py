dbaf0204149bf450ac56085971446fbe50262c8a
Nakashima Ryosuke <r.nakashima.a10@m.kyushu-u.ac.jp>
2024-07-09 01:12:21 +0900

    feat: add a module

--- package/make_chebyshev.py
+++ package/make_chebyshev.py
@@ -1,4 +1,4 @@
-"""Makes data of the Chebyshev polynomials"""
+"""Makes data of Chebyshev polynomials"""
 
 import cmath
 
@@ -13,14 +13,14 @@ def chebyshev(n_degree: int,
     Parameters
     -----
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of a Chebyshev polynomial
     s_complex : complex
         The position of a point
 
     Returns
     -----
     value_chebyshev : complex
-        The value of the Chebyshev polynomial
+        The value of a Chebyshev polynomial at a point
 
     """
 
@@ -39,24 +39,27 @@ def chebyshev_d(n_degree: int,
     Parameters
     -----
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of a Chebyshev polynomial
     s_complex : complex
         The position of a point
 
     Returns
     -----
     value_chebyshev_d : complex
-        The value of the first derivative of the Chebyshev polynomial
+        The value of the first derivative of a Chebyshev polynomial at
+        a point
 
     """
 
     value_chebyshev_d: complex = 0
-    if n_degree % 2 == 0:
-        for i_n in range(int(n_degree/2)):
-            value_chebyshev_d += chebyshev(2*i_n+1, s_complex)
+    if n_degree == 0:
+        value_chebyshev_d = 0
+    elif n_degree % 2 == 0:
+        for i_n in range(1, n_degree, 2):
+            value_chebyshev_d += chebyshev(i_n, s_complex)
     else:
-        for i_n in range(int(n_degree/2)):
-            value_chebyshev_d += chebyshev(2*i_n+2, s_complex)
+        for i_n in range(2, n_degree, 2):
+            value_chebyshev_d += chebyshev(i_n, s_complex)
         value_chebyshev_d += chebyshev(0, s_complex) / 2
     #
 
@@ -75,31 +78,79 @@ def chebyshev_d2(n_degree: int,
     Parameters
     -----
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of a Chebyshev polynomial
     s_complex : complex
         The position of a point
 
     Returns
     -----
     value_chebyshev_d2 : complex
-        The value of the second derivative of the Chebyshev polynomial
+        The value of the second derivative of a Chebyshev polynomial at
+        a point
 
     """
 
     value_chebyshev_d2: complex = 0
-    if n_degree % 2 == 0:
-        for i_n in range(int(n_degree/2)):
-            value_chebyshev_d2 += ((n_degree**2)-((2*i_n)**2)) \
-                * chebyshev(2*i_n, s_complex)
+    if 0 <= n_degree <= 1:
+        value_chebyshev_d2 = 0
+    elif n_degree % 2 == 0:
+        for i_n in range(2, n_degree-1, 2):
+            value_chebyshev_d2 += ((n_degree**2)-(i_n**2)) \
+                * chebyshev(i_n, s_complex)
         value_chebyshev_d2 \
-            -= (n_degree**2) * chebyshev(0, s_complex) / 2
+            += (n_degree**2) * chebyshev(0, s_complex) / 2
     else:
-        for i_n in range(int(n_degree/2)):
-            value_chebyshev_d2 += ((n_degree**2)-((2*i_n+1)**2)) \
-                * chebyshev(2*i_n+1, s_complex)
+        for i_n in range(1, n_degree-1, 2):
+            value_chebyshev_d2 += ((n_degree**2)-(i_n**2)) \
+                * chebyshev(i_n, s_complex)
     #
 
     value_chebyshev_d2 *= n_degree
 
     return value_chebyshev_d2
+#
+
+
+@njit
+def chebyshev_d3(n_degree: int,
+                 s_complex: complex) -> complex:
+    """Calculates the value of the third derivative of a Chebyshev
+    polynomial at a point
+
+    Parameters
+    -----
+    n_degree : int
+        The degree of a Chebyshev polynomial
+    s_complex : complex
+        The position of a point
+
+    Returns
+    -----
+    value_chebyshev_d3 : complex
+        The value of the third derivative of a Chebyshev polynomial at
+        a point
+
+    """
+
+    value_chebyshev_d3: complex = 0
+    if 0 <= n_degree <= 2:
+        value_chebyshev_d3 = 0
+    elif n_degree % 2 == 0:
+        for i_n in range(1, n_degree-2, 2):
+            value_chebyshev_d3 += (((n_degree+1)**2)-(i_n**2)) \
+                * (((n_degree-1)**2)-(i_n**2)) \
+                * chebyshev(i_n, s_complex)
+    else:
+        for i_n in range(2, n_degree-2, 2):
+            value_chebyshev_d3 += (((n_degree+1)**2)-(i_n**2)) \
+                * (((n_degree-1)**2)-(i_n**2)) \
+                * chebyshev(i_n, s_complex)
+        value_chebyshev_d3 \
+            += ((n_degree+1)**2) * ((n_degree-1)**2) \
+            * chebyshev(0, s_complex) / 2
+    #
+
+    value_chebyshev_d3 *= n_degree / 4
+
+    return value_chebyshev_d3
 #
